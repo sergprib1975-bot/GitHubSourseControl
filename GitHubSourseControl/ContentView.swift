027374e2f66f4550c7963255581cb70bd15ea1c7
SergPrib <sergprib1975@gmail.com>
2026-01-25 23:14:42 +0300

    Initial Commit

---
+++ GitHubSourseControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubSourseControl
+//
+//  Created by MarsRover2020 on 25.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubSourseControl/GitHubSourseControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSourseControlApp.swift
+//  GitHubSourseControl
+//
+//  Created by MarsRover2020 on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSourseControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
