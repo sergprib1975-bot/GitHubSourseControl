1c53531767dab086f6039ae2b573d9de075ddbbc
SergPrib <sergprib1975@gmail.com>
2026-01-26 20:21:11 +0300

    add homeview

---
+++ GitHubSourseControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitHubSourseControl
+//
+//  Created by MarsRover2020 on 26.01.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
