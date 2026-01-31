77a547f8bf78b3f8cbfab2a50185a1731492de76
SergPrib <sergprib1975@gmail.com>
2026-01-31 21:15:48 +0300

    add text "branching"

--- GitHubSourseControl/ContentView.swift
+++ GitHubSourseControl/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             Text("Swift Laboratory")
                 .font(.title)
             Rectangle()
+            Text("Branching")
         }
         .padding()
     }
